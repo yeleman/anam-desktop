4e9ba28cd21317be3be8950382d9e33b4ebc86ff
renaud gaudin <rgaudin@gmail.com>
2017-06-07 16:14:02 +0000

    fixed oracle encoding on windows

--- launcher.py
+++ launcher.py
@@ -21,21 +21,20 @@ def get_oraclfolder():
 ORACLE_HOME = os.path.join(os.getcwd(),
                            'oraclient_{}'.format(get_oraclfolder()))
 
+environ = {}
+environ['LD_LIBRARY_PATH'] = ORACLE_HOME
+environ['ORACLE_HOME'] = ""
+environ['NLS_LANG'] = ".AL32UTF8"
+
 if platform.system() == 'Darwin':
-    environ = {}
     environ['DYLD_LIBRARY_PATH'] = ORACLE_HOME
-    environ['LD_LIBRARY_PATH'] = ORACLE_HOME
-    environ['ORACLE_HOME'] = ORACLE_HOME
-    environ['NLS_LANG'] = ".AL32UTF8"
     try:
         os.execve(sys.executable, [sys.executable, 'anamdesk.py'], environ)
     except Exception as exc:
         print("Failed re-exec: {}".format(exc))
         sys.exit(1)
 else:
-    os.environ.update({'LD_LIBRARY_PATH': ORACLE_HOME,
-                       'ORACLE_HOME': "",
-                       'PATH': "{};{}".format(os.environ['PATH'],
-                                              ORACLE_HOME)})
+    environ['PATH'] = "{};{}".format(os.environ['PATH'], ORACLE_HOME)
+    os.environ.update(environ)
     from anamdesktop.entry import main
     main()
